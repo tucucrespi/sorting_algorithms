8266f6a0185503fa1e092b026917dde093cda357
tucucrespi <tucucrespi@hotmail.com>
2021-02-03 15:08:23 +0000

    add

---
+++ 2-selection_sort.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort array using selection sort algorithm
+ * @array: array
+ * @size: array size
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+unsigned int i, j, min, temporal;
+
+if (array == NULL || size < 2)
+return;
+
+/*  0   1   2   3   4   5   6   7   8   9 */
+/* {19, 48, 99, 71, 13, 52, 96, 73, 86, 7}; */
+
+for (i = 0; i < size; i++)
+{
+/* min = 3; array[min] = 99 */
+
+min = i;
+for (j = i + 1; j < size; j++)
+{
+/* j = 4; array[j] = 13 */
+
+/* si 99 > 13? */
+if (array[min] > array[j])
+/* Entonces min ahora vale 4 */
+min = j;
+}
+
+/* si min (4) es distinto 3? */
+if (min != i)
+{
+/* Entonces array[min] (13) */
+temporal = array[min];
+/* Array[min] (13) > array[i] = 99 */
+array[min] = array[i];
+/* array[i] = 13 */
+array[i] = temporal;
+
+/*  0   1   2   3   4   5   6   7   8   9 */
+/* {19, 48, 71, 13, 99, 52, 96, 73, 86, 7}; */
+
+print_array(array, size);
+}
+}
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * _split - Split the array and takes the last element as pivot
+ * @array: Array
+ * @first: first element of array
+ * @last: last element of array
+ * @size: size of array
+ * Return: index of new part
+ **/
+int _split(int *array, int first, int last, size_t size)
+{
+int pivot;
+int i = first;
+int j, temp;
+
+pivot = array[last];
+for (j = first; j < last; j++)
+{
+if (array[j] <= pivot)
+{
+
+temp = array[j];
+array[j] = array[i];
+array[i] = temp;
+if (i != j)
+print_array(array, size);
+i++;
+
+}
+}
+
+temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+if (i != j)
+print_array(array, size);
+
+return (i);
+}
+
+/**
+ * quick_sort_parts - sort array in two parts
+ * @array: array
+ * @first: first index
+ * @last: last index
+ * @size: size of array
+ * Return: Nothing
+ */
+void quick_sort_parts(int *array, int first, int last, size_t size)
+{
+
+int pivot;
+
+if (first < last)
+{
+pivot = _split(array, first, last, size);
+quick_sort_parts(array, first, (pivot - 1), size);
+quick_sort_parts(array, (pivot + 1), last, size);
+}
+}
+
+/**
+ * quick_sort -Sort an array using quick_sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ **/
+void quick_sort(int *array, size_t size)
+{
+if (array == NULL || size < 2)
+return;
+
+quick_sort_parts(array, 0, size - 1, size);
+}
